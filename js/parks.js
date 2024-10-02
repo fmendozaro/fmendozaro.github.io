9963aaa6992636806d48c323e7b526f77bb13063
Fer Mendoza <fernandoza@gmail.com>
2024-10-01 22:14:44 -0700

    update: poark list

--- js/parks.js
+++ js/parks.js
@@ -2,16 +2,23 @@
 const ROWC = {
     "Parks": [
         {
-            "name": "java",
-            "mapsLink": 2013,
+            "name": "Crissy Field Beach",
+            "mapsLink": "https://www.google.com/maps/place/Crissy+Field/@37.8040257,-122.4650165,17z/data=!3m1!4b1!4m6!3m5!1s0x808586dc8525b355:0x5b15912201aa3600!8m2!3d37.8040257!4d-122.4650165!16zL20vMDYxdGp4?entry=ttu&g_ep=EgoyMDI0MDkyOS4wIKXMDSoASAFQAw%3D%3D",
             "img": "", 
             "coords":""
         },
         {
-            "name": "java",
-            "mapsLink": 2013,
+            "name": "Lafeyette Park",
+            "mapsLink": "https://g.co/kgs/RB5sPMc",
+            "img": "", 
+            "coords":""
+        },
+        {
+            "name": "Alta Plaza",
+            "mapsLink": "https://g.co/kgs/UQYf4dm",
             "img": "", 
             "coords":""
         }
+        
     ]
 };
